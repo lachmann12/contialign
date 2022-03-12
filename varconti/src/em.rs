b68ea1f2e52dfef2126a54abce80296de200e0c0
Alexander Lachmann <32603869+lachmann12@users.noreply.github.com>
2022-03-12 18:18:27 -0500

    em filter

--- varconti/src/em.rs
+++ varconti/src/em.rs
@@ -4,10 +4,10 @@ use std::collections::BinaryHeap;
 use std::hash::{Hash, Hasher};
 use std::collections::hash_map::DefaultHasher;
 
-pub fn expection_maximization(input: Vec<Vec<u32>>, transcript_length: usize) -> Vec<f32> {
+extern crate libc;
+use std::mem;
 
-    
-    let now = Instant::now();
+pub fn expection_maximization(input: Vec<Vec<u32>>, transcript_length: usize) -> Vec<f32> {
 
     let mut ec_counts: HashMap<u32, f32> = HashMap::new();
     let mut ec_map: HashMap<u32, Vec<u32>> = HashMap::new();
@@ -28,42 +28,62 @@ pub fn expection_maximization(input: Vec<Vec<u32>>, transcript_length: usize) ->
     for k in 0..ec_keys.len() {
         let key = &(ec_keys[k] as u32);
         let v = ec_map.get(key).unwrap();
-        let count = ec_counts.get(key).unwrap();
-        ec_count_vec.push(count);
-        ec_map_vec.push(v);
+        
+        if v.len() < 100 {
+            let mut vu = vec![];
+            for i in v {
+                vu.push(*i as usize);
+            }
+            let count = ec_counts.get(key).unwrap();
+            ec_count_vec.push(count);
+            ec_map_vec.push(vu);
+        }
     }
 
+    println!("em classes: {}",ec_map_vec.len());
+
+    let now = Instant::now();
+
     for i in 0..100 {
-        let mut new_final_counts = vec![0.0; transcript_length];
+        
+        let mut new_final_counts: Vec<f32> = vec![0.0; transcript_length];
         
         for k in 0..ec_map_vec.len() {
             
-            let v = ec_map_vec[k];
+            let v = &ec_map_vec[k];
             let temp_count = ec_count_vec[k];
 
             if v.len() == 1 {
-                new_final_counts[v[0] as usize] += temp_count;
+                new_final_counts[v[0]] += temp_count;
             }
             else {
                 let mut gene_sum = 0.0;
                 for j in 0..v.len() {
-                    gene_sum += final_counts[v[j] as usize];
+                    gene_sum += final_counts[v[j]];
                 }
+                
                 for j in 0..v.len() {
-                    let temp = final_counts[v[j] as usize]/gene_sum;
-                    if temp < 0.0000000001 {
-                        new_final_counts[v[j] as usize] = 0.0;
+                    let temp = final_counts[v[j]]/gene_sum;
+                    if temp < 0.0000000000 {
+                        new_final_counts[v[j]] = 0.0;
                     }
                     else{
-                        new_final_counts[v[j] as usize] += temp_count*final_counts[v[j] as usize]/gene_sum;
+                        new_final_counts[v[j]] += temp_count*temp;
                     }
                 }
             }
         }
+
         final_counts = new_final_counts;
     }
 
     println!("EM time: {}", now.elapsed().as_millis());
+
+    let mut final_count_total = 0.0;
+    for j in 0..final_counts.len() {
+        final_count_total += final_counts[j];
+    }
+    println!("total counts: {}", final_count_total);
 
     return final_counts;
 }
