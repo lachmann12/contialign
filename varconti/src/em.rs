b517bf3a5120dc6e3df1686667bd696c6f81dc84
Alexander Lachmann <alexander.lachmann@gmail.com>
2022-03-16 20:38:08 -0400

    best so far

--- varconti/src/em.rs
+++ varconti/src/em.rs
@@ -59,12 +59,12 @@ pub fn expection_maximization(input: Vec<Vec<u32>>, transcript_length: usize, tr
             else {
                 let mut gene_sum = 0.0;
                 for j in 0..v.len() {
-                    gene_sum += transcript_lengths[v[j]]/final_counts[v[j]];
+                    gene_sum += final_counts[v[j]]/(transcript_lengths[v[j]] as f32);
                 }
                 
                 for j in 0..v.len() {
-                    let temp = final_counts[v[j]]/(gene_sum*transcript_lengths[v[j]]);
-                    if temp < 0.0000000000 {
+                    let temp = (final_counts[v[j]]/gene_sum)/(transcript_lengths[v[j]] as f32);
+                    if temp < 0.00000000001 {
                         new_final_counts[v[j]] = 0.0;
                     }
                     else{
